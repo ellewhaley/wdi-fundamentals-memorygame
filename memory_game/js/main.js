469fbc04350c59b8f4f83a99f75aa9bafe5b1874
Eleanor Whaley <eleanorwhaley@eleanors-mbp-2.home>
2019-02-21 10:36:05 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,20 +1,25 @@
-console.log("Up and running!");
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
 
-cardsInPlay.push('CardOne');
-cardsInPlay.push('CardTwo');
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
-if (cardsInPlay[0] === cardsInPlay[1]){
+var checkForMatch = function () {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]){
 	alert("You found a match!");
 } else {
 	alert("Sorry, try again.");
+		}
+	}
 }
+
+var flipCard = function(cardId) {
+console.log("User flipped " + cardId);
+cardsInPlay.push(cardId);
+checkForMatch();
 }
+
+flipCard(cards[0]);
+flipCard(cards[2]);
+
+
+
