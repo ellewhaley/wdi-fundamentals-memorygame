a8c572e4a4687f69a9111b540a62bb329d838b08
Eleanor Whaley <eleanorwhaley@eleanors-mbp-2.home>
2019-02-28 09:34:41 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -28,37 +28,44 @@ var cards = [
 
 var cardsInPlay = [];
 
+//check for match when two cards are selected.
+var checkForMatch = function() {
+
+    if (cardsInPlay[0] ===  cardsInPlay[1]) {
+        alert("You found a match");
+       } else {
+        alert("Sorry, try again");
+       }
+}
+
+//Flip card to show front image.
 var flipCard = function() {
+
 var cardId = this.getAttribute('data-id');
+
 this.setAttribute('src', cards[cardId].cardImage);
-console.log("User flipped " + cards[cardId].rank);
+
 cardsInPlay.push(cards[cardId].rank);
-console.log(cards[cardId].cardImage);
-console.log(cards[cardsId].suit);
+
+if (cardsInPlay.length === 2) {
 
 checkForMatch();
-};
-
-var checkForMatch = function() {
-    if (cardsInPlay.length === 2) {
-        if (cardsInPlay[0] ===  cardsInPlay[1]) {
-            alert("You found a match");
-        } else {
-            alert("Sorry, try again");
-        };
-    };
-};
-
-var createBoard = function () {
-	for (var i = 0; i < cards.length; i++) {
- var cardElement = document.createElement('img');
- cardElement.setAttribute('src', 'images/back.png');
- cardElement.setAttribute('data-id', i);
- cardElement.addEventListener('click', flipCard);
- document.getElementById('game-board').appendChild(cardElement);
 	}
 };
 
+//Load card images to the board.
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+
+ 		var cardElement = document.createElement('img');
+ 		cardElement.setAttribute('src', 'images/back.png');
+ 		cardElement.setAttribute('data-id', i);
+ 		cardElement.addEventListener('click', flipCard);
+ 		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+//Call createBoard function.
 createBoard();
 
 
